a8f7cc5b26398cbb7c91b7522e786a8ce3da86c2
FlannyH <flannyha@gmail.com>
2023-08-31 04:04:54 +0200

    Removed unused timers for now

--- source/timer.c
+++ source/timer.c
@@ -1,5 +1,6 @@
 #include "timer.h"
 #include "music.h"
+#include <stdio.h>
 
 static int timer0_count = 0;
 static int timer1_count = 0;
@@ -20,11 +21,7 @@ static void timer2_handler(void) {
 
 void setup_timers() {
 	TIMER_CTRL(0) = 0x0160; // Dotclock input, repeated IRQ on overflow
-	TIMER_CTRL(1) = 0x0160; // Hblank input, repeated IRQ on overflow
-	TIMER_CTRL(2) = 0x0260; // CLK/8 input, repeated IRQ on overflow
 	EnterCriticalSection();
 	InterruptCallback(IRQ_TIMER0, &timer0_handler);
-	InterruptCallback(IRQ_TIMER1, &timer1_handler);
-	InterruptCallback(IRQ_TIMER2, &timer2_handler);
 	ExitCriticalSection();
 }
