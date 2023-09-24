b5beccc1120f75e596c5c4b31bb82ead5ea90bd2
FlannyH <flannyha@gmail.com>
2023-09-24 23:20:57 +0200

    Update compile_tools.bat

--- compile_tools.bat
+++ compile_tools.bat
@@ -3,8 +3,9 @@ D:
 cd D:/Projects/Git/obj2psx
 cargo build --release
 copy D:\Projects\Git\obj2psx\target\release\deps\obj2psx.exe D:\Projects\Git\ShooterPSX\tools\obj2psx.exe /Y
-cd D:/Projects/Git/ShooterPSX
 
 cd D:/Projects/Git/psx_vislist_generator
 cargo build --release
 copy D:\Projects\Git\psx_vislist_generator\target\release\deps\psx_vislist_generator.exe D:\Projects\Git\ShooterPSX\tools\psx_vislist_generator.exe /Y
+
+cd D:/Projects/Git/ShooterPSX
