5d6b168874faba497507223215d414dfdafd2b77
Lily Haverlag <flannyha@gmail.com>
2024-07-25 20:27:09 +0200

    Rename random to random_u32 to avoid name conflict

--- source/random.h
+++ source/random.h
@@ -3,12 +3,8 @@
 
 #include <stdint.h>
 
-#ifdef random
-#undef random
-#endif
-
 static uint32_t random_state = 0x26082023;
-static uint32_t random() {
+static uint32_t random_u32() {
     random_state ^= random_state << 13;
     random_state ^= random_state >> 17;
     random_state ^= random_state << 5;
@@ -16,7 +12,7 @@ static uint32_t random() {
 }
 
 static int32_t random_range(int32_t min_inclusive, int32_t max_exclusive) {
-    return ((int32_t)random() % (max_exclusive - min_inclusive) + min_inclusive);
+    return ((int32_t)random_u32() % (max_exclusive - min_inclusive) + min_inclusive);
 }
 
 #endif
