9581ae08152c4a796bae825062430d7bbf873c4b
Lily Haverlag <flannyha@gmail.com>
2024-07-31 20:20:44 +0200

    NDS: Implement renderer_begin_frame

--- source/nds/renderer.c
+++ source/nds/renderer.c
@@ -3,6 +3,8 @@
 
 #include <nds.h>
 
+vec3_t camera_dir;
+
 void renderer_init(void) {
     videoSetMode(MODE_0_3D);
     glInit();
@@ -12,4 +14,25 @@ void renderer_init(void) {
     glViewport(0, 0, 255, 191);
     vramSetBankA(VRAM_A_TEXTURE);
     gluPerspective(90, 256.0 / 192.0, 0.1, 40);
+}
+
+int16_t angle_to_16(int angle) {
+    return (angle >> 1) & 0xFFFF;
+}
+
+void renderer_begin_frame(const transform_t* camera_transform) {
+    // Set view matrix
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotateXi(angle_to_16(camera_transform->rotation.x));
+    glRotateYi(angle_to_16(-camera_transform->rotation.y));
+    glRotateZi(angle_to_16(camera_transform->rotation.z));
+    glTranslatef32(-camera_transform->position.x, -camera_transform->position.y, -camera_transform->position.z);
+
+    // Fetch view matrix
+    int view_matrix[9];
+    glGetFixed(GL_GET_MATRIX_VECTOR, &view_matrix)
+    camera_dir.x = view_matrix[2]; // todo: verify that these are correct
+    camera_dir.y = view_matrix[5];
+    camera_dir.z = view_matrix[8];
 }
