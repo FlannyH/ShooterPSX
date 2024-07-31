7fdaf9220c4f6fff437c8a6563ed037c94cc29cb
Lily Haverlag <flannyha@gmail.com>
2024-07-31 18:10:39 +0200

    NDS: Add dummy implementations for music code

---
+++ source/nds/music.c
@@ -0,0 +1,10 @@
+#include "music.h"
+
+void music_test_sound() {} // todo: implement this
+void music_test_instr_region(int region) {} // todo: implement this
+void music_load_soundbank(const char* path) {} // todo: implement this
+void music_load_sequence(const char* path) {} // todo: implement this
+void music_play_sequence(int section) {} // todo: implement this
+void music_tick(int delta_time) {} // todo: implement this
+void music_set_volume(int volume) {} // todo: implement this
+void music_stop(void) {} // todo: implement this
