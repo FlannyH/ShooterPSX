44c25d80d50364ce803259106225eaff41fb1ed5
Lily Haverlag <flannyha@gmail.com>
2024-08-01 05:36:17 +0200

    Add todo implementations for renderer functions

--- source/nds/renderer.c
+++ source/nds/renderer.c
@@ -4,6 +4,12 @@
 #include <nds.h>
 
 vec3_t camera_dir;
+int tex_level_start = 0;
+int tex_entity_start = 0;
+int tex_weapon_start = 0;
+int res_x = 256;
+int vsync_enable = 1;
+int is_pal = 0;
 
 void renderer_init(void) {
     videoSetMode(MODE_0_3D);
@@ -40,4 +46,52 @@ void renderer_begin_frame(const transform_t* camera_transform) {
 void renderer_end_frame(void) {
     // todo: update delta time
     glFlush(0);
+}
+
+void renderer_draw_mesh_shaded(const mesh_t* mesh, const transform_t* model_transform, int local, int tex_id_offset) {
+    TODO()
+}
+
+void renderer_draw_2d_quad(vec2_t tl, vec2_t tr, vec2_t bl, vec2_t br, vec2_t uv_tl, vec2_t uv_br, pixel32_t color, int depth, int texture_id, int is_page) {
+    TODO()
+}
+
+void renderer_apply_fade(int fade_level) {
+    TODO()
+}
+
+void renderer_debug_draw_line(vec3_t v0, vec3_t v1, pixel32_t color, const transform_t* model_transform) {
+    TODO()
+}
+
+void renderer_upload_texture(const texture_cpu_t* texture, uint8_t index) {
+    TODO()
+}
+
+void renderer_upload_8bit_texture_page(const texture_cpu_t* texture, const uint8_t index) {
+    TODO()
+}
+
+void renderer_set_video_mode(int is_pal) {
+    TODO()
+}
+
+int renderer_get_delta_time_raw(void) {
+    TODO()
+}
+
+int renderer_get_delta_time_ms(void) {
+    TODO()
+}
+
+int renderer_convert_dt_raw_to_ms(int dt_raw) {
+    TODO()
+}
+
+int renderer_should_close(void) {
+    TODO()
+}
+
+void renderer_set_depth_bias(int bias) {
+    TODO()
 }
