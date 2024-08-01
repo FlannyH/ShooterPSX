a51186c663a6e521708bd235b8a4e11831a29be9
Lily Haverlag <flannyha@gmail.com>
2024-08-02 01:13:32 +0200

    Replace unimplemented functions in music.c with TODO()

--- source/nds/music.c
+++ source/nds/music.c
@@ -1,10 +1,36 @@
 #include "music.h"
 
-void music_test_sound(void) {} // todo: implement this
-void music_test_instr_region(int region) {} // todo: implement this
-void music_load_soundbank(const char* path) {} // todo: implement this
-void music_load_sequence(const char* path) {} // todo: implement this
-void music_play_sequence(int section) {} // todo: implement this
-void music_tick(int delta_time) {} // todo: implement this
-void music_set_volume(int volume) {} // todo: implement this
-void music_stop(void) {} // todo: implement this
+#include "common.h"
+
+void music_test_sound(void) {
+    TODO()
+}
+
+void music_test_instr_region(int region) {
+    TODO()
+}
+
+void music_load_soundbank(const char* path) {
+    TODO()
+}
+
+void music_load_sequence(const char* path) {
+    TODO()
+}
+
+void music_play_sequence(int section) {
+    TODO()
+}
+
+void music_tick(int delta_time) {
+    TODO()
+}
+
+void music_set_volume(int volume) {
+    TODO()
+}
+
+void music_stop(void) {
+ 
+    TODO()
+}
