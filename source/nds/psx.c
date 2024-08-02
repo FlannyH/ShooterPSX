1287c556e571a1373ba9a27895986a60c916de35
Lily Haverlag <flannyha@gmail.com>
2024-08-03 00:28:11 +0200

    NDS: Fix trig functions

--- source/nds/psx.c
+++ source/nds/psx.c
@@ -8,28 +8,28 @@
 int hisin(const int a) {
     // sinLerp takes in a value from -32768 to +32767
     // sinLerp returns a 4.12 fixed point number, same as PSX
-    return (int)sinLerp((s16)(a / 2));
+    return (int)sinLerp((s16)(a / 4));
 }
 
 // Angle value: -65536 to +65535
 int hicos(const int a) {
     // cosLerp takes in a value from -32768 to +32767
     // cosLerp returns a 4.12 fixed point number, same as PSX
-    return (int)cosLerp((s16)(a / 2));
+    return (int)cosLerp((s16)(a / 4));
 }
 
 // Angle value: -2048 to +2047
 int isin(const int a) {
     // cosLerp takes in a value from -32768 to +32767
     // cosLerp returns a 4.12 fixed point number, same as PSX
-    return (int)cosLerp((s16)(a * 16));
+    return (int)cosLerp((s16)(a * 8));
 }
 
 // Angle value: -2048 to +2047
 int icos(const int a) {
     // cosLerp takes in a value from -32768 to +32767
     // cosLerp returns a 4.12 fixed point number, same as PSX
-    return (int)cosLerp((s16)(a * 16));
+    return (int)cosLerp((s16)(a * 8));
 }
 
 char* FntFlush(int id) {
