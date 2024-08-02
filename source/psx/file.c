192d481f715b359f7e7bcafbfe4c063074e82b74
Lily Haverlag <flannyha@gmail.com>
2024-08-02 03:49:19 +0200

    PS1: Fix file loading

--- source/psx/file.c
+++ source/psx/file.c
@@ -7,10 +7,22 @@
 #include "memory.h"
 
 int file_read(const char* path, uint32_t** destination, size_t* size, int on_stack, stack_t stack) {
+    // Convert to upper case
+    const size_t length = strlen(path) + 1; // Include the null terminator
+    char* new_path = mem_stack_alloc(length, STACK_TEMP);
+    for (size_t i = 0; i < length; ++i) {
+        if (path[i] >= 'a' && path[i] <= 'z') {
+            new_path[i] = path[i] - ('a' - 'A');
+        }
+        else {
+            new_path[i] = path[i];
+        }
+    }
+
     // Search for the file on disc - return false if it does not exist
     CdlFILE file;
-    if (!CdSearchFile(&file, path)) {
-        printf("Can't find file %s!", path);
+    if (!CdSearchFile(&file, new_path)) {
+        printf("Can't find file %s!", new_path);
         return 0;
     }
 
