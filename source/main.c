27f48630f01b4c04c7ba03af63ef18547b909f44
Lily Haverlag <flannyha@gmail.com>
2024-09-07 01:05:48 +0200

    Temporarily bump memory stacks to beyond the size of PS1 RAM

--- source/main.c
+++ source/main.c
@@ -45,6 +45,9 @@ state_t prev_state = STATE_NONE;
 state_vars_t state;
 
 int main(void) {
+	#ifdef _PSX
+	InitHeap((void*)0x80200000, 0x00400000);
+	#endif
 	mem_init();
 	mem_debug();
 	// Init systems
