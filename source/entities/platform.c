85cb9bf46cbce947a8ef62a128dd699b31ba2d3f
Lily Haverlag <flannyha@gmail.com>
2024-10-18 21:02:48 +0200

    Implement basic entity functionality for platform

--- source/entities/platform.c
+++ source/entities/platform.c
@@ -23,6 +23,34 @@ entity_platform_t* entity_platform_new(void) {
 }
 
 void entity_platform_update(int slot, player_t* player, int dt) {
+	(void)dt;
+
+	entity_platform_t* platform = (entity_platform_t*)&entity_pool[slot * entity_pool_stride];
+	vec3_t platform_pos = platform->entity_header.position;
+	vec3_t player_pos = vec3_sub(player->position, (vec3_t){0, 200 * COL_SCALE, 0});
+
+	// Make this platform solid based on the mesh bounding box
+	if (platform->entity_header.mesh == NULL) {
+		platform->entity_header.mesh = model_find_mesh(entity_models, "29_platform_test_horizontal");
+	}
+	// Render
+	transform_t render_transform;
+	render_transform.position.x = -platform_pos.x / COL_SCALE;
+	render_transform.position.y = -platform_pos.y / COL_SCALE;
+	render_transform.position.z = -platform_pos.z / COL_SCALE;
+	render_transform.rotation.x = -platform->entity_header.rotation.x;
+	render_transform.rotation.y = -platform->entity_header.rotation.y;
+	render_transform.rotation.z = -platform->entity_header.rotation.z;
+	render_transform.scale.x = platform->entity_header.scale.x;
+	render_transform.scale.y = platform->entity_header.scale.x;
+	render_transform.scale.z = platform->entity_header.scale.x;
+#ifdef _LEVEL_EDITOR
+	drawing_entity_id = slot;
+#endif
+	renderer_draw_mesh_shaded(platform->entity_header.mesh, &render_transform, 0, 0, tex_entity_start);
 }
+
 void entity_platform_on_hit(int slot, int hitbox_index) {
+	(void)slot;
+	(void)hitbox_index;
 }
