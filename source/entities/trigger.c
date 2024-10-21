caba71c121933bd138ad57b67421b7ff1d2e72b5
Lily Haverlag <flannyha@gmail.com>
2024-10-22 01:51:10 +0200

    Fix formatting

--- source/entities/trigger.c
+++ source/entities/trigger.c
@@ -1,18 +1,18 @@
 #include "trigger.h"
 
 entity_trigger_t* entity_trigger_new(void) {
-	entity_trigger_t* entity = (entity_trigger_t*)&entity_pool[entity_alloc(ENTITY_TRIGGER) * entity_pool_stride];
-	entity->entity_header.position = (vec3_t){0, 0, 0};
-	entity->entity_header.rotation = (vec3_t){0, 0, 0};
-	entity->entity_header.scale = (vec3_t){ONE, ONE, ONE};
+    entity_trigger_t* entity = (entity_trigger_t*)&entity_pool[entity_alloc(ENTITY_TRIGGER) * entity_pool_stride];
+    entity->entity_header.position = (vec3_t){0, 0, 0};
+    entity->entity_header.rotation = (vec3_t){0, 0, 0};
+    entity->entity_header.scale = (vec3_t){ONE, ONE, ONE};
 }
 
 void entity_trigger_update(int slot, player_t *player, int dt) {
-	(void)dt;
+    (void)dt;
 
-	entity_trigger_t* trigger = (entity_trigger_t*)&entity_pool[slot * entity_pool_stride];
-	const vec3_t trigger_pos = trigger->entity_header.position;
-	const vec3_t player_pos = vec3_sub(player->position, (vec3_t){0, 200 * COL_SCALE, 0});
+    entity_trigger_t* trigger = (entity_trigger_t*)&entity_pool[slot * entity_pool_stride];
+    const vec3_t trigger_pos = trigger->entity_header.position;
+    const vec3_t player_pos = vec3_sub(player->position, (vec3_t){0, 200 * COL_SCALE, 0});
     const vec3_t trigger_scale_half = vec3_muls(trigger->entity_header.scale, ONE/2);
 
     const entity_collision_box_t box = {
