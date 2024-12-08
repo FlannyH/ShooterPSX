db227abb490b0a49f83e8ce570345ff0590925e9
Lily Haverlag <flannyha@gmail.com>
2024-12-08 15:39:52 +0100

    Move struct definitions to header to prepare for cross-platform implementations

--- source/windows/file.c
+++ source/windows/file.c
@@ -5,27 +5,6 @@
 #include <string.h>
 #include <stdio.h>
 
-#define MAGIC_FSFA 0x41465346
-typedef struct {
-    uint32_t file_magic; // Must be equal to MAGIC_FSFA
-    uint32_t n_items; // How many items does this file contain
-    uint32_t items_offset; // Offset (bytes) to the items list, relative to the start of the file
-    uint32_t data_offset; // Offset (bytes) to the data section, relative to the start of the file
-} fsfa_header_t;
-
-typedef enum {
-    FSFA_ITEM_TYPE_FOLDER = 0,
-    FSFA_ITEM_TYPE_FILE = 1,
-} fsfa_item_type_t;
-
-typedef struct {
-    uint8_t type; // value of type `fsfa_item_type_t`
-    char name[12]; // The item's name. This string is null terminated if the length is below 12, and not null terminated if the length is equal to 12
-    char extension[3]; // The item's file extension. This field is ignored for folders.
-    uint32_t offset; // Folders: First child index. Files: Offset to file contents, relative to the start of the binary section.
-    uint32_t size; // Folders: how many items are stored in the folder. Files: file size in bytes
-} fsfa_item_t;
-
 FILE* file_archive = NULL;
 fsfa_header_t archive_header;
 fsfa_item_t* item_table = NULL;
