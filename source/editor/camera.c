a7dc703fc26e9d1859154340bcff52b732f165bf
Lily Haverlag <flannyha@gmail.com>
2025-01-12 21:17:06 +0100

    Improve debug camera

--- source/editor/camera.c
+++ source/editor/camera.c
@@ -10,30 +10,42 @@ debug_camera_t debug_camera_new(void) {
             .rotation = vec3_from_scalar(0),
             .scale = vec3_from_scalar(ONE)
         },
+        .position = vec3_from_scalar(0),
         .velocity = vec3_from_scalar(0),
-        .move_speed = ONE,
-        .acceleration = 0,
+        .max_speed = 2048,
+        .drag = 32,
+        .acceleration = 32,
     };
 }
 
-void debug_camera_update(debug_camera_t* self, const int dt_ms) {
+void debug_camera_update(debug_camera_t* self, const int dt_ms, const int register_input) {
+    // Change max move speed based on scroll input
+    if (input_mouse_scroll() > 0) {
+        self->max_speed = scalar_mul(self->max_speed, scalar_from_float(1.1f));
+    }
+    if (input_mouse_scroll() < 0) {
+        self->max_speed = scalar_mul(self->max_speed, scalar_from_float(1.0f / 1.1f));
+    }
+
     // Moving forwards and backwards
-    {
-        self->velocity.x += hisin(self->transform.rotation.y) * input_left_stick_y(0) * (self->acceleration) >> 16;
-        self->velocity.z += hicos(self->transform.rotation.y) * input_left_stick_y(0) * (self->acceleration) >> 16;
+    if (register_input) {
+        self->velocity.x += hisin(self->transform.rotation.y) * input_left_stick_y(0) * (self->acceleration * dt_ms) >> 16;
+        self->velocity.z -= hicos(self->transform.rotation.y) * input_left_stick_y(0) * (self->acceleration * dt_ms) >> 16;
 
         // Strafing left and right
-        self->velocity.x -= hicos(self->transform.rotation.y) * input_left_stick_x(0) * (self->acceleration) >> 16;
-        self->velocity.z += hisin(self->transform.rotation.y) * input_left_stick_x(0) * (self->acceleration) >> 16;
+        self->velocity.x += hicos(self->transform.rotation.y) * input_left_stick_x(0) * (self->acceleration * dt_ms) >> 16;
+        self->velocity.z += hisin(self->transform.rotation.y) * input_left_stick_x(0) * (self->acceleration * dt_ms) >> 16;
 
         // Moving up and down
-        self->velocity.y += input_held(PAD_SQUARE, 0) ? self->acceleration : 0;
-        self->velocity.y -= input_held(PAD_CROSS, 0) ? self->acceleration : 0;
+        self->velocity.y -= input_held(PAD_SQUARE, 0) ? self->acceleration * dt_ms * 127 : 0;
+        self->velocity.y += input_held(PAD_CROSS, 0) ? self->acceleration * dt_ms * 127 : 0;
     }
 
     // Implement drag
     {
         // Calculate magnitude for velocity on X and Z axes
+        const scalar_t max_speed = self->max_speed;
+        const scalar_t drag = self->drag * dt_ms;
         scalar_t velocity_x = self->velocity.x;
         scalar_t velocity_z = self->velocity.z;
         const scalar_t velocity_x2 = scalar_mul(velocity_x, velocity_x);
@@ -46,12 +58,12 @@ void debug_camera_update(debug_camera_t* self, const int dt_ms) {
         velocity_z = scalar_div(velocity_z, velocity_scalar);
 
         // Clamp magnitude
-        if (velocity_scalar > walking_max_speed) {
-            velocity_scalar = walking_max_speed - walking_drag;
+        if (velocity_scalar > max_speed) {
+            velocity_scalar = max_speed - drag;
         }
         // Apply drag
-        else if (velocity_scalar > walking_drag) {
-            velocity_scalar -= walking_drag;
+        else if (velocity_scalar > drag) {
+            velocity_scalar -= drag;
         }
         else {           
             velocity_scalar = 0;
@@ -64,13 +76,27 @@ void debug_camera_update(debug_camera_t* self, const int dt_ms) {
         // Put it back in the velocity component
         self->velocity.x = velocity_x;
         self->velocity.z = velocity_z;
+
+        // Now for the Y axis
+        scalar_t velocity_y_scalar = abs(self->velocity.y);
+        if (velocity_y_scalar > max_speed) {
+            velocity_y_scalar = max_speed - drag;
+        }
+        else if (velocity_y_scalar > drag) {
+            velocity_y_scalar -= drag;
+        }
+        else {           
+            velocity_y_scalar = 0;
+        }
+        if (self->velocity.y < 0) velocity_y_scalar *= -1;
+        self->velocity.y = velocity_y_scalar;
     }
 
     // Move the player based on velocity
-    self->transform.position = vec3_add(self->transform.position, vec3_muls(self->velocity, (dt_ms * ONE) / 1000));
+    self->position = vec3_add(self->position, vec3_muls(self->velocity, dt_ms * ONE));
 
     // Look up and down
-    self->transform.rotation.x -= (int32_t)(input_right_stick_y(0)) * (mouse_sensitivity * dt_ms) >> 12;
+    self->transform.rotation.x -= (int32_t)(input_mouse_movement_y() * 8) * (mouse_sensitivity) >> 12;
     if (self->transform.rotation.x > 32768) {
         self->transform.rotation.x = 32768;
     }
@@ -79,5 +105,10 @@ void debug_camera_update(debug_camera_t* self, const int dt_ms) {
     }
 
     // Look left and right
-    self->transform.rotation.y += (int32_t)(input_right_stick_x(0)) * (mouse_sensitivity * dt_ms) >> 12;
+    self->transform.rotation.y += (int32_t)(input_mouse_movement_x() * 8) * (mouse_sensitivity) >> 12;
+
+    // Transform from a position in collision space to a view offset in graphics space
+    self->transform.position.x = -self->position.x * (4096 / COL_SCALE);
+    self->transform.position.y = -self->position.y * (4096 / COL_SCALE);
+    self->transform.position.z = -self->position.z * (4096 / COL_SCALE);
 }
