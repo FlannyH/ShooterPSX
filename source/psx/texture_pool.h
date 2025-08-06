0c1bd22f459a58cbc2bb9039c0549c8e395ee9fb
Lily Haverlag <flannyha@gmail.com>
2025-08-06 19:35:09 +0200

    Implement variable block texture allocator

---
+++ source/psx/texture_pool.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+void texture_pool_init(uint32_t pool_index, uint16_t top, uint16_t left, uint32_t resolution);
+int texture_pool_alloc(uint32_t pool_index, uint32_t width, uint32_t height); // returns texture id within the pool
+RECT texture_pool_rect(uint32_t pool_index, int texture_id);
