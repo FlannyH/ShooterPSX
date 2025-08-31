e3cf3c731e3ea099d2fe0b2c77a587417c2a3f98
Lily Haverlag <flannyha@gmail.com>
2025-08-31 15:22:39 +0200

    Format DebugCallbackFunc switch statements

--- source/pc/renderer.c
+++ source/pc/renderer.c
@@ -86,10 +86,10 @@ static void DebugCallbackFunc(GLenum source, GLenum type, GLuint id,
 															const GLchar *message, const GLvoid *userParam) {
 	(void)length;
 	(void)userParam;
-	// Skip some less useful info
-	if (id ==
-			131218) // http://stackoverflow.com/questions/12004396/opengl-debug-context-performance-warning
-		return;
+
+	// Skip some less useful info 
+	// http://stackoverflow.com/questions/12004396/opengl-debug-context-performance-warning
+	if (id == 131218) return;
 
 	char *sourceString;
 	char *typeString;
@@ -98,84 +98,30 @@ static void DebugCallbackFunc(GLenum source, GLenum type, GLuint id,
 	// The AMD variant of this extension provides a less detailed classification
 	// of the error, which is why some arguments might be "Unknown".
 	switch (source) {
-	case GL_DEBUG_SOURCE_API: {
-		sourceString = "API";
-		break;
-	}
-	case GL_DEBUG_SOURCE_APPLICATION: {
-		sourceString = "Application";
-		break;
-	}
-	case GL_DEBUG_SOURCE_WINDOW_SYSTEM: {
-		sourceString = "Window System";
-		break;
-	}
-	case GL_DEBUG_SOURCE_SHADER_COMPILER: {
-		sourceString = "Shader Compiler";
-		break;
-	}
-	case GL_DEBUG_SOURCE_THIRD_PARTY: {
-		sourceString = "Third Party";
-		break;
-	}
-	case GL_DEBUG_SOURCE_OTHER: {
-		sourceString = "Other";
-		break;
-	}
-	default: {
-		sourceString = "Unknown";
-		break;
-	}
+	case GL_DEBUG_SOURCE_API:				sourceString = "API";				break;
+	case GL_DEBUG_SOURCE_APPLICATION:		sourceString = "Application";		break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:		sourceString = "Window System";		break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER:	sourceString = "Shader Compiler";	break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:		sourceString = "Third Party";		break;
+	case GL_DEBUG_SOURCE_OTHER:				sourceString = "Other";				break;
+	default:								sourceString = "Unknown";			break;
 	}
 
 	switch (type) {
-	case GL_DEBUG_TYPE_ERROR: {
-		typeString = "Error";
-		break;
-	}
-	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: {
-		typeString = "Deprecated Behavior";
-		break;
-	}
-	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: {
-		typeString = "Undefined Behavior";
-		break;
-	}
-	case GL_DEBUG_TYPE_PORTABILITY_ARB: {
-		typeString = "Portability";
-		break;
-	}
-	case GL_DEBUG_TYPE_PERFORMANCE: {
-		typeString = "Performance";
-		break;
-	}
-	case GL_DEBUG_TYPE_OTHER: {
-		typeString = "Other";
-		break;
-	}
-	default: {
-		typeString = "Unknown";
-		break;
-	}
+	case GL_DEBUG_TYPE_ERROR: 				typeString = "Error";				break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: typeString = "Deprecated Behavior";	break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: 	typeString = "Undefined Behavior";	break;
+	case GL_DEBUG_TYPE_PORTABILITY_ARB: 	typeString = "Portability";			break;
+	case GL_DEBUG_TYPE_PERFORMANCE: 		typeString = "Performance";			break;
+	case GL_DEBUG_TYPE_OTHER: 				typeString = "Other";				break;
+	default: 								typeString = "Unknown";				break;
 	}
 
 	switch (severity) {
-	case GL_DEBUG_SEVERITY_HIGH: {
-		severityString = "High";
-		break;
-	}
-	case GL_DEBUG_SEVERITY_MEDIUM: {
-		severityString = "Medium";
-		break;
-	}
-	case GL_DEBUG_SEVERITY_LOW: {
-		severityString = "Low";
-		break;
-	}
-	default: {
-		severityString = "Unknown";
-		return;
-	}
+	case GL_DEBUG_SEVERITY_HIGH: 	severityString = "High"; 	break;
+	case GL_DEBUG_SEVERITY_MEDIUM: 	severityString = "Medium"; 	break;
+	case GL_DEBUG_SEVERITY_LOW: 	severityString = "Low"; 	break;
+	default: 						severityString = "Unknown"; return;
 	}
 
 	printf("GL Debug Callback:\n source: %i:%s \n type: %i:%s \n id: %i \n "
