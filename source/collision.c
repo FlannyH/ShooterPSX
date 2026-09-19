8eea7af42adb80535d96cf5ed9224b59bc9233d8
Lily Haverlag <flannyha@gmail.com>
2026-09-20 00:30:44 +0200

    Attempt at fixing GJK and EPA

--- source/collision.c
+++ source/collision.c
@@ -1,39 +1,71 @@
+#include <stdio.h>
+
 #include "collision.h"
 #include "level.h"
+#include "mesh.h"
 #include "math/fixed_point.h"
 #include "structs.h"
 #include "math/vec3.h"
 
+#include <assert.h>
 #include <string.h>
-#define MAX_EPA_TRIES 32
+#define MAX_EPA_TRIES 8
+
+void dump_current_simplex(size_t i, convex_hull_mesh_t* polytope, size_t closest_face) {
+    char path[1024] = {0};
+    snprintf(path, 1023, "simplex_%i.obj", i);
+    FILE* o = fopen(&path[0], "wb");
+    fprintf(o, "o simplex_%lu\n", i);
+    for (size_t i = 0; i < polytope->n_vertices; ++i) {
+        fprintf(o, "v %.3f %.3f %.3f\n",
+            ((float)polytope->vertices[i].x) / 4096.f,
+            ((float)polytope->vertices[i].y) / 4096.f,
+            ((float)polytope->vertices[i].z) / 4096.f
+        );
+    }
+    for (size_t i = 0; i < polytope->n_faces; ++i) {
+        fprintf(o, "vn %.3f %.3f %.3f\n",
+            ((float)polytope->faces[i].normal.x) / 4096.f,
+            ((float)polytope->faces[i].normal.y) / 4096.f,
+            ((float)polytope->faces[i].normal.z) / 4096.f
+        );
+    }
+    for (size_t i = 0; i < polytope->n_faces; ++i) {
+        fprintf(o, "f %i//%i %i//%i %i//%i\n",
+           polytope->faces[i].a + 1, i+1,
+           polytope->faces[i].b + 1, i+1,
+           polytope->faces[i].c + 1, i+1
+        );
+    }
+
+    fprintf(o, "o simplex_%lu_closest_face\n", i);
+    fprintf(o, "v %.3f %.3f %.3f\n",
+        ((float)polytope->vertices[polytope->faces[closest_face].a].x) / 4096.f,
+        ((float)polytope->vertices[polytope->faces[closest_face].a].y) / 4096.f,
+        ((float)polytope->vertices[polytope->faces[closest_face].a].z) / 4096.f
+    );
+    fprintf(o, "v %.3f %.3f %.3f\n",
+        ((float)polytope->vertices[polytope->faces[closest_face].a].x) / 4096.f,
+        ((float)polytope->vertices[polytope->faces[closest_face].a].y) / 4096.f,
+        ((float)polytope->vertices[polytope->faces[closest_face].a].z) / 4096.f
+    );
+    fprintf(o, "v %.3f %.3f %.3f\n",
+        ((float)polytope->vertices[polytope->faces[closest_face].a].x) / 4096.f,
+        ((float)polytope->vertices[polytope->faces[closest_face].a].y) / 4096.f,
+        ((float)polytope->vertices[polytope->faces[closest_face].a].z) / 4096.f
+    );
+    fprintf(o, "vn %.3f %.3f %.3f\n",
+        ((float)polytope->faces[closest_face].normal.x) / 4096.f,
+        ((float)polytope->faces[closest_face].normal.y) / 4096.f,
+        ((float)polytope->faces[closest_face].normal.z) / 4096.f
+    );
+    fprintf(o, "f 1//1 2//1 3//1\n");
+    fclose(o);
+}
 
 // directions are normalized to make it more predictable with fixed point precision
 
-typedef struct {
-    size_t a, b, c;
-    vec3_t normal;
-} face_t;
-
-typedef struct {
-    size_t a, b;
-} edge_t;
-
-typedef struct {
-    // gjk
-    vec3_t a, b, c, d;
-    vec3_t dir;
-    scalar_t next_vtx_count;
-
-    // epa
-    vec3_t vertices[320];
-    size_t n_vertices;
-    face_t faces[320];
-    size_t n_faces;
-    edge_t edges[320];
-    size_t n_edges;
-} gjk_state_t;
-
-gjk_state_t s = {0};
+#define SUPPORT(a, b, dir) vec3_sub(support_shape((a), (dir)), support_shape((b), vec3_neg(dir)))
 
 vec3_t support_sphere(sphere_t sphere, vec3_t direction) {
     direction = vec3_normalize(direction);
@@ -107,248 +139,221 @@ vec3_t support_shape(shape_t* shape, vec3_t direction) {
 
 #define SIMILAR_DIR(a, b) (vec3_dot((a), (b)) > 0)
 
-int handle_triangle_case(void) {
-    printf("triangle case\n");
+int handle_line_case(convex_hull_mesh_t* polytope, vec3_t* dir) {
+    // printf("line case\n");
+    const vec3_t a = polytope->vertices[0];
+    const vec3_t b = polytope->vertices[1];
 
-    const vec3_t ab = vec3_normalize(vec3_sub(s.b, s.a));
-    const vec3_t ac = vec3_normalize(vec3_sub(s.c, s.a));
-    const vec3_t ao = vec3_normalize(vec3_neg(s.a));
+    const vec3_t ab = vec3_normalize(vec3_sub(b, a));
+    const vec3_t ao = vec3_normalize(vec3_neg(a));
 
-    const vec3_t nrm = vec3_cross(ab, ac);
-
-    if (SIMILAR_DIR(vec3_cross(ab, nrm), ao)) { // origin outside near edge AB
-        s.next_vtx_count = 3;
-        s.c = s.b;
-        s.b = s.a;
-        s.dir = vec3_cross(vec3_cross(ab, ao), ab);
-        return 0;
+    if (SIMILAR_DIR(ab, ao)) {
+        // polytope->vertices[0] = a;
+        // polytope->vertices[1] = b;
+        // polytope->n_vertices = 2;
+        *dir = vec3_cross_lh(vec3_cross_lh(ab, ao), ab);
     }
-    if (SIMILAR_DIR(vec3_cross(nrm, ac), ao)) { // origin outside near edge AC
-        s.next_vtx_count = 3;
-        s.c = s.c;
-        s.b = s.a;
-        s.dir = vec3_cross(vec3_cross(ac, ao), ac);
-        return 0;
-    }
-    if (SIMILAR_DIR(nrm, ao)) { // origin inside above triangle
-        s.next_vtx_count = 4;
-        s.d = s.c;
-        s.c = s.b;
-        s.b = s.a;
-        s.dir = nrm;
-    }
-    else { // origin inside below or on triangle
-        s.next_vtx_count = 4;
-        s.d = s.b;
-        s.c = s.c;
-        s.b = s.a;
-        s.dir = vec3_neg(nrm);
+    else {
+        polytope->vertices[0] = a;
+        polytope->n_vertices = 1;
+        *dir = ao;
     }
 
     return 0;
 }
 
-int handle_tetrahedron_case(void) {
-    printf("tetrahedron case\n");
+int handle_triangle_case(convex_hull_mesh_t* polytope, vec3_t* dir) {
+    // printf("triangle case\n");
 
-    const vec3_t ab = vec3_normalize(vec3_sub(s.b, s.a));
-    const vec3_t ac = vec3_normalize(vec3_sub(s.c, s.a));
-    const vec3_t ad = vec3_normalize(vec3_sub(s.d, s.a));
-    const vec3_t ao = vec3_normalize(vec3_neg(s.a));
+    const vec3_t a = polytope->vertices[0];
+    const vec3_t b = polytope->vertices[1];
+    const vec3_t c = polytope->vertices[2];
 
-    const vec3_t abc = vec3_cross(ab, ac);
-    const vec3_t acd = vec3_cross(ac, ad);
-    const vec3_t adb = vec3_cross(ad, ab);
+    const vec3_t ab = vec3_normalize(vec3_sub(b, a));
+    const vec3_t ac = vec3_sub(c, a);
+    const vec3_t ao = vec3_neg(a);
+
+    const vec3_t abc = vec3_normalize(vec3_cross_lh(ab, ac));
+
+    if (SIMILAR_DIR(vec3_cross_lh(abc, ac), ao)) {
+        if (SIMILAR_DIR(ac, ao)) {
+            // polytope->vertices[0] = a;
+            polytope->vertices[1] = c;
+            polytope->n_vertices = 2;
+            *dir = vec3_normalize(vec3_cross_lh(vec3_cross_lh(ac, ao), ac));
+        }
+        else {
+            // polytope->vertices[0] = a;
+            // polytope->vertices[1] = b;
+            polytope->n_vertices = 2;
+            // *dir = s.dir;
+            return handle_line_case(polytope, dir);
+        }
+    }
+    else {
+        if (SIMILAR_DIR(vec3_cross_lh(ab, abc), ao)) {
+            // polytope->vertices[0] = a;
+            // polytope->vertices[1] = b;
+            polytope->n_vertices = 2;
+            // *dir = s.dir;
+            return handle_line_case(polytope, dir);
+        }
+        else {
+            if (SIMILAR_DIR(abc, ao)) {
+                // polytope->vertices[0] = a;
+                // polytope->vertices[1] = b;
+                // polytope->vertices[2] = c;
+                // polytope->n_vertices = 3;
+                *dir = abc;
+            }
+            else {
+                // polytope->vertices[0] = a;
+                polytope->vertices[1] = c;
+                polytope->vertices[2] = b;
+                // polytope->n_vertices = 3;
+                *dir = vec3_neg(abc);
+            }
+        }
+    }
+
+    return 0;
+}
+
+int handle_tetrahedron_case(convex_hull_mesh_t* polytope, vec3_t* dir) {
+    // printf("tetrahedron case\n");
+
+    const vec3_t a = polytope->vertices[0];
+    const vec3_t b = polytope->vertices[1];
+    const vec3_t c = polytope->vertices[2];
+    const vec3_t d = polytope->vertices[3];
+
+    const vec3_t ab = vec3_normalize(vec3_sub(b, a));
+    const vec3_t ac = vec3_normalize(vec3_sub(c, a));
+    const vec3_t ad = vec3_normalize(vec3_sub(d, a));
+    const vec3_t ao = vec3_neg(a);
+
+    vec3_t abc = vec3_cross_lh(ab, ac);
+    vec3_t acd = vec3_cross_lh(ac, ad);
+    vec3_t adb = vec3_cross_lh(ad, ab);
 
     if (SIMILAR_DIR(abc, ao)) {
-        return handle_triangle_case();
+        // polytope->vertices[0] = a;
+        // polytope->vertices[1] = b;
+        // polytope->vertices[2] = c;
+        polytope->n_vertices = 3;
+        // *dir = *dir;
+        return handle_triangle_case(polytope, dir);
     }
     if (SIMILAR_DIR(acd, ao)) {
-        s.b = s.c;
-        s.c = s.d;
-        return handle_triangle_case();
+        // polytope->vertices[0] = a;
+        polytope->vertices[1] = c;
+        polytope->vertices[2] = d;
+        polytope->n_vertices = 3;
+        // *dir = *dir;
+        return handle_triangle_case(polytope, dir);
     }
     if (SIMILAR_DIR(adb, ao)) {
-        s.c = s.b;
-        s.b = s.d;
-        return handle_triangle_case();
+        // polytope->vertices[0] = a;
+        polytope->vertices[1] = d;
+        polytope->vertices[2] = b;
+        polytope->n_vertices = 3;
+        // *dir = *dir;
+        return handle_triangle_case(polytope, dir);
     }
     return 1;
 }
 
 // returns if simplex surrounds origin, and updates the simplex to be ready for the next point
-int check_origin_and_update_simplex(void) {
-    switch (s.next_vtx_count) {
-        case 3: return handle_triangle_case();
-        case 4: return handle_tetrahedron_case();
+int update_simplex(convex_hull_mesh_t* polytope, vec3_t* dir) {
+    switch (polytope->n_vertices) {
+        case 2: return handle_line_case(polytope, dir);
+        case 3: return handle_triangle_case(polytope, dir);
+        case 4: return handle_tetrahedron_case(polytope, dir);
         default: return 0;
     }
 }
 
-int gjk(shape_t* shape1, shape_t* shape2) {
+int gjk(convex_hull_mesh_t* polytope, shape_t* shape1, shape_t* shape2) {
+    if (!shape1) return 0;
+    if (!shape2) return 0;
     if (shape1->type == SHAPE_NONE) return 0;
     if (shape2->type == SHAPE_NONE) return 0;
-    memset(&s, 0, sizeof(gjk_state_t));
 
     // random initial direction
-    s.dir = vec3_from_floats(1.0f, 0.0f, 0.0f);
-    s.c = vec3_sub(support_shape(shape1, s.dir), support_shape(shape2, vec3_neg(s.dir)));
+    vec3_t dir = vec3_from_floats(1.0f, 0.0f, 0.0f);
+    vec3_t support = SUPPORT(shape1, shape2, dir);
+    polytope->vertices[0] = support;
+    polytope->n_vertices = 1;
 
     // next point is towards the origin
-    s.dir = vec3_normalize(vec3_neg(s.c));
-    s.b = vec3_sub(support_shape(shape1, s.dir), support_shape(shape2, vec3_neg(s.dir)));
+    dir = vec3_normalize(vec3_neg(support));
 
-    // didn't pass the origin -> no collision
-    if (vec3_dot(s.b, s.dir) <= 0.0f) return 0;
+    int attempts = 16;
+    while (attempts--) {
+        support = SUPPORT(shape1, shape2, dir);
 
-    // find direction to look for the 3rd point
-    const vec3_t bc = vec3_normalize(vec3_sub(s.c, s.b));
-    const vec3_t bo = vec3_normalize(vec3_neg(s.b));
-    s.dir = vec3_cross(vec3_cross(bc, bo), bc);
-    s.next_vtx_count = 3;
+        if (vec3_dot(support, dir) <= 0.0f) {
+            return 0;
+        }
 
-    for (int attempts = 0; attempts < 64; ++attempts) {
-        s.a = vec3_sub(support_shape(shape1, s.dir), support_shape(shape2, vec3_neg(s.dir)));
-        if (vec3_dot(s.a, s.dir) <= 0.0f) return 0; // didn't pass the origin -> no collision
-        if (check_origin_and_update_simplex()) return 1; // if our simplex surrounds origin -> yes collision
+        polytope->vertices[3] = polytope->vertices[2];
+        polytope->vertices[2] = polytope->vertices[1];
+        polytope->vertices[1] = polytope->vertices[0];
+        polytope->vertices[0] = support;
+        polytope->n_vertices++;
+        assert(polytope->n_vertices <= CONVEX_HULL_LIMIT);
+
+        if (update_simplex(polytope, &dir)) {
+            return 1;
+        }
     }
+
     return 0;
 }
 
-void remove_edge_at_index(size_t index) {
-    edge_t* dst = &s.edges[index];
-    edge_t* src = &s.edges[index + 1];
-    size_t count = (--s.n_edges) - index;
-    memmove(dst, src, count * sizeof(edge_t));
-}
+vec3_t epa(convex_hull_mesh_t* polytope, shape_t* shape1, shape_t* shape2) {
+    // take the simplex geturned from gjk as the start polytope
+    assert(polytope->n_vertices == 4);
 
-void remove_face_at_index(size_t index) {
-    face_t* dst = &s.faces[index];
-    face_t* src = &s.faces[index + 1];
-    size_t count = (--s.n_faces) - index;
-    memmove(dst,src, count * sizeof(face_t));
-}
+    polytope->faces[0] = (face_t){ 0, 1, 2, {0}, INT32_MAX };
+    polytope->faces[1] = (face_t){ 0, 3, 1, {0}, INT32_MAX };
+    polytope->faces[2] = (face_t){ 0, 2, 3, {0}, INT32_MAX };
+    polytope->faces[3] = (face_t){ 1, 3, 2, {0}, INT32_MAX };
+    polytope->n_faces = 4;
 
-void add_unique_edge(edge_t edge) {
-    size_t i = 0;
+    vec3_t interior_point = {0};
+    size_t closest_face_index = get_face_normals(polytope, 0, interior_point);
 
-    while (i < s.n_edges) {
-        const edge_t existing_edge = s.edges[i];
-        const int is_reverse = (edge.a == existing_edge.b) && (edge.b == existing_edge.a);
-        if (is_reverse) {
-            break;
-        }
-        ++i;
-    }
-    if (i != s.n_edges) {
-        remove_edge_at_index(i);
-    }
-    else {
-        s.edges[s.n_edges++] = edge;
-    }
-}
+    for (size_t attempt = 1; attempt <= MAX_EPA_TRIES; ++attempt) {
+        const face_t closest_face = polytope->faces[closest_face_index];
+        const vec3_t support = SUPPORT(shape1, shape2, closest_face.normal);
+        const scalar_t distance_support_to_origin = vec3_dot(closest_face.normal, support);
 
-int find_in_edges(size_t* index, const edge_t edge) {
-    for (size_t i = 0; i < s.n_edges; ++i) {
-        if (s.edges[i].a == edge.a
-         && s.edges[i].b == edge.b) {
-             *index = i;
-             return 1;
-         }
-    }
-    return 0;
-}
 
-vec3_t calculate_normal(size_t face_id) {
-    const vec3_t ab = vec3_normalize(vec3_sub(s.vertices[s.faces[face_id].b], s.vertices[s.faces[face_id].a]));
-    const vec3_t ac = vec3_normalize(vec3_sub(s.vertices[s.faces[face_id].c], s.vertices[s.faces[face_id].a]));
-    return vec3_normalize(vec3_cross(ab, ac));
-}
+        dump_current_simplex(attempt, polytope, closest_face_index);
 
-void epa_unique_edge(edge_t edge) {
-    size_t index = 0;
-    if (find_in_edges(&index, (edge_t){edge.b, edge.a})) {
-        remove_edge_at_index(index);
-    }
-    else {
-        s.edges[s.n_edges++] = edge;
-    }
-}
-
-vec3_t epa(shape_t* shape1, shape_t* shape2) {
-    s.vertices[0] = s.a;
-    s.vertices[1] = s.b;
-    s.vertices[2] = s.c;
-    s.vertices[3] = s.d;
-    s.n_vertices = 4;
-
-    s.faces[0] = (face_t){ 0, 1, 2, {0} };
-    s.faces[1] = (face_t){ 0, 3, 1, {0} };
-    s.faces[2] = (face_t){ 0, 2, 3, {0} };
-    s.faces[3] = (face_t){ 1, 3, 2, {0} };
-    s.n_faces = 4;
-
-    for (size_t i = 0; i < s.n_faces; ++i) {
-        s.faces[i].normal = calculate_normal(i);
-    }
-
-    scalar_t min_distance = INT32_MAX;
-    vec3_t min_normal = {0};
-
-    while (min_distance == INT32_MAX) {
-        // find closest face to the origin
-        for (size_t i_face = 0; i_face < s.n_faces; ++i_face) {
-            const vec3_t nrm = s.faces[i_face].normal;
-            const vec3_t a = s.vertices[s.faces[i_face].a];
-            scalar_t distance = vec3_dot(nrm, a);
-            if (distance < min_distance) {
-                min_distance = distance;
-                min_normal = nrm;
-            }
+        if ((distance_support_to_origin - closest_face.distance) < SCALAR(0.1f)) {
+            goto eh_close_enough;
         }
 
-        // if there is no distance, do nothing
-        if (min_distance == 0) return (vec3_t){0};
+        convex_hull_expand(polytope, support);
 
-        // if the support point tells us there's more minkowski diff in that direction -> extendo
-        const vec3_t support = vec3_sub(support_shape(shape1, min_normal), support_shape(shape2, vec3_neg(min_normal)));
-        const scalar_t distance = vec3_dot(min_normal, support);
-        if (scalar_abs(distance - min_distance) > SCALAR(1.0f)) {
-            min_distance = INT32_MAX;
-
-            // remove all faces in the same direction and rebuild polytope
-            s.n_edges = 0;
-            size_t i_face = 0;
-            while (i_face < s.n_faces) {
-                if (vec3_dot(s.faces[i_face].normal, vec3_sub(support, s.vertices[s.faces[i_face].a])) > 0) {
-                    epa_unique_edge((edge_t){s.faces[i_face].a, s.faces[i_face].b});
-                    epa_unique_edge((edge_t){s.faces[i_face].b, s.faces[i_face].c});
-                    epa_unique_edge((edge_t){s.faces[i_face].c, s.faces[i_face].a});
-                    remove_face_at_index(i_face);
-                    continue;
-                }
-                ++i_face;
-            }
-
-            const size_t new_vtx_i = s.n_vertices++;
-            s.vertices[new_vtx_i] = support;
-
-            for (size_t edge_i = 0; edge_i < s.n_edges; ++edge_i) {
-                const size_t new_face_i = s.n_faces++;
-                s.faces[new_face_i].a = s.edges[edge_i].a;
-                s.faces[new_face_i].b = s.edges[edge_i].b;
-                s.faces[new_face_i].c = new_vtx_i;
-                s.faces[new_face_i].normal = calculate_normal(new_face_i);
-                if (vec3_dot(s.faces[new_face_i].normal, s.vertices[s.faces[new_face_i].a]) < 0) {
-                    s.faces[new_face_i].b = new_vtx_i;
-                    s.faces[new_face_i].c = s.edges[edge_i].b;
-                    s.faces[new_face_i].normal = vec3_neg(s.faces[new_face_i].normal);
-                }
-            }
-        }
+        closest_face_index = get_face_normals(polytope, 0, interior_point);
     }
 
-    return vec3_muls(min_normal, min_distance + SCALAR(0.003f));
+    // dump_current_simplex(MAX_EPA_TRIES, polytope);
+    // the closest face seems to lie close enough to the minkowski difference's boundaries
+    // we can assume this will give us the real penetration direction and distance, so let's return that
+    eh_close_enough:
+
+    if (polytope->faces[closest_face_index].distance > SCALAR(100)) {
+        printf("seems sus.\n");
+    }
+    if (polytope->faces[closest_face_index].distance < SCALAR(0)) {
+        printf("seems turbo <0 sus.\n");
+    }
+
+    return vec3_muls(polytope->faces[closest_face_index].normal, polytope->faces[closest_face_index].distance + SCALAR(0.01f));
 }
 
 void move_sphere(sphere_t* shape, vec3_t move_by) {
--- source/collision.h
+++ source/collision.h
@@ -7,8 +7,26 @@
 
 #include <stdint.h>
 
-int gjk(shape_t* shape1, shape_t* shape2);
-vec3_t epa(shape_t* shape1, shape_t* shape2);
+typedef struct {
+    size_t a, b, c;
+    vec3_t normal;
+    scalar_t distance;
+} face_t;
+
+typedef struct {
+    size_t a, b;
+} edge_t;
+
+typedef struct {
+    #define CONVEX_HULL_LIMIT 1024
+    vec3_t vertices[CONVEX_HULL_LIMIT];
+    size_t n_vertices;
+    face_t faces[CONVEX_HULL_LIMIT];
+    size_t n_faces;
+} convex_hull_mesh_t;
+
+int gjk(convex_hull_mesh_t* polytope, shape_t* shape1, shape_t* shape2);
+vec3_t epa(convex_hull_mesh_t* polytope, shape_t* shape1, shape_t* shape2);
 
 void move_sphere(sphere_t* shape, vec3_t move_by);
 void move_capsule(capsule_t* shape, vec3_t move_by);
