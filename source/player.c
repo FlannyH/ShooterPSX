bd06ea584a506ead64b85ba899283506759d6ea2
Lily Haverlag <flannyha@gmail.com>
2026-09-21 20:57:20 +0200

    Work on player code

--- source/player.c
+++ source/player.c
@@ -1,19 +1,23 @@
 #include "player.h"
 #include "collision.h"
-#include "common.h"
+#include "math/vec2.h"
+#include "math/vec3.h"
 
 #ifdef _PSX
 #include <psxpad.h>
 #endif
 
-#include "common.h"
+#ifdef _PC
+#include "pc/psx.h"
+#endif
+
+#ifdef _NDS
+#include "nds/psx.h"
+#endif
+
 #include "input.h"
-#include "entity.h"
-#include "common.h"
 #include "music.h"
 #include "random.h"
-
-#include <string.h>
 
 #define FOOTSTEP_TIMER_MAX 350
 
@@ -52,13 +56,22 @@ void collide(player_t* self, level_t* level) {
 
     convex_hull_mesh_t polytope = {0};
 
-    for (size_t i = 0; (i < level->n_shapes) && level->shapes; ++i) {
+    for (int i = 0; (i < level->n_shapes) && level->shapes; ++i) {
         if (level->shapes[i].type == SHAPE_NONE) continue;
         if (gjk(&polytope, &level->shapes[i], &player)) {
             printf("colliding with shape %i\n", i);
             const vec3_t penetration = epa(&polytope, &level->shapes[i], &player);
             // const vec3_t old_pos = player.capsule.a;
             move_shape(&player, penetration);
+
+            if (penetration.y > 0) {
+                self->is_grounded = 1;
+            }
+
+            // get part of the vector along the move axis
+            const vec3_t nrm_pen = vec3_normalize(penetration);
+            const scalar_t length_along_nrm_pen = vec3_dot(self->velocity, nrm_pen);
+            self->velocity = vec3_sub(self->velocity, vec3_muls(nrm_pen, length_along_nrm_pen));
         }
     }
 
@@ -68,126 +81,95 @@ void collide(player_t* self, level_t* level) {
     self->position.y += player_radius;
 }
 
-void apply_gravity(player_t* self, const int dt_ms) {
+void apply_gravity(player_t* self, const scalar_t dt) {
     if (self->is_grounded) return;
 
-    self->velocity.y = (self->velocity.y + gravity * dt_ms);
-    if (self->velocity.y > terminal_velocity_up) {
-        self->velocity.y = terminal_velocity_up;
+    self->velocity.y += scalar_mul(gravity, dt) * PLAYER_VELOCITY_PRECISION;
+    if (self->velocity.y > terminal_velocity_up * PLAYER_VELOCITY_PRECISION) {
+        self->velocity.y = terminal_velocity_up * PLAYER_VELOCITY_PRECISION;
     }
-    else if (self->velocity.y < terminal_velocity_down) {
-        self->velocity.y = terminal_velocity_down;
-    }
-}
-
-void handle_stick_input(player_t* self, const int dt_ms) {
-    const scalar_t curr_acceleration = (self->is_grounded) ? (walking_acceleration * dt_ms) : ((walking_acceleration * dt_ms) / air_acceleration_divider);
-
-    // todo: merge parts of this code so we only do one set of additions to the player pos rot and vel
-
-    if (input_mouse_connected()) {
-        // Moving forwards and backwards
-        self->velocity.x += trig_sin(self->rotation.y) * input_left_stick_y(0) * (curr_acceleration) >> 16;
-        self->velocity.z += trig_cos(self->rotation.y) * input_left_stick_y(0) * (curr_acceleration) >> 16;
-
-        // Strafing left and right
-        self->velocity.x += trig_cos(self->rotation.y) * input_left_stick_x(0) * (curr_acceleration) >> 16;
-        self->velocity.z -= trig_sin(self->rotation.y) * input_left_stick_x(0) * (curr_acceleration) >> 16;
-
-        // Look up and down
-        self->rotation.x -= (int32_t)(input_mouse_movement_y()) * (mouse_sensitivity) >> 12;
-        if (self->rotation.x > 32768) {
-            self->rotation.x = 32768;
-        }
-        if (self->rotation.x < -32768) {
-            self->rotation.x = -32768;
-        }
-
-        // Look left and right
-        self->rotation.y -= (int32_t)(input_mouse_movement_x()) * (mouse_sensitivity) >> 12;
-    }
-
-    if (input_has_analog(0)) {
-        // Moving forwards and backwards
-        self->velocity.x += trig_sin(self->rotation.y) * input_left_stick_y(0) * (curr_acceleration) >> 16;
-        self->velocity.z += trig_cos(self->rotation.y) * input_left_stick_y(0) * (curr_acceleration) >> 16;
-
-        // Strafing left and right
-        self->velocity.x += trig_cos(self->rotation.y) * input_left_stick_x(0) * (curr_acceleration) >> 16;
-        self->velocity.z -= trig_sin(self->rotation.y) * input_left_stick_x(0) * (curr_acceleration) >> 16;
-
-        // Look up and down
-        self->rotation.x -= (int32_t)(input_right_stick_y(0)) * (stick_sensitivity * dt_ms) >> 12;
-        if (self->rotation.x > SCALAR(0.22)) {
-            self->rotation.x = SCALAR(0.22);
-        }
-        if (self->rotation.x < -SCALAR(0.22)) {
-            self->rotation.x = -SCALAR(0.22);
-        }
-
-        // Look left and right
-        self->rotation.y += (int32_t)(input_right_stick_x(0)) * (stick_sensitivity * dt_ms) >> 12;
-
-#ifdef _DEBUG
-        if (input_held(PAD_UP, 0)) {
-            self->position.y += ONE * dt_ms;
-            self->velocity.y = 0;
-        }
-        if (input_held(PAD_DOWN, 0)) {
-            self->position.y -= ONE * dt_ms;
-            self->velocity.y = 0;
-        }
-#endif
-    } else {
-        // Look left and right
-        const int32_t dpad_x = ((int32_t)(input_held(PAD_RIGHT, 0) != 0) * 127) + ((int32_t)(input_held(PAD_LEFT, 0) != 0) * -127);
-        self->rotation.y += dpad_x * (stick_sensitivity * dt_ms) >> 12;
-
-        // Moving forwards and backwards
-        const int32_t dpad_y = ((int32_t)(input_held(PAD_UP, 0) != 0) * 127) + ((int32_t)(input_held(PAD_DOWN, 0) != 0) * -127);
-        self->velocity.x += trig_sin(self->rotation.y) * dpad_y * (curr_acceleration) >> 16;
-        self->velocity.z += trig_cos(self->rotation.y) * dpad_y * (curr_acceleration) >> 16;
-
-        // Strafing left and right
-        const int32_t shoulder_x = ((int32_t)(input_held(PAD_L1, 0) != 0) * 127) + ((int32_t)(input_held(PAD_R1, 0) != 0) * -127);
-        self->velocity.x -= trig_cos(self->rotation.y) * shoulder_x * (curr_acceleration) >> 16;
-        self->velocity.z += trig_sin(self->rotation.y) * shoulder_x * (curr_acceleration) >> 16;
+    else if (self->velocity.y < terminal_velocity_down * PLAYER_VELOCITY_PRECISION) {
+        self->velocity.y = terminal_velocity_down * PLAYER_VELOCITY_PRECISION;
     }
 }
 
-void handle_drag(player_t* self, const int dt_ms) {
-    // Calculate magnitude for velocity on X and Z axes
-    scalar_t velocity_x = self->velocity.x;
-    scalar_t velocity_z = self->velocity.z;
-    const scalar_t velocity_x2 = scalar_mul(velocity_x, velocity_x);
-    const scalar_t velocity_z2 = scalar_mul(velocity_z, velocity_z);
-    const scalar_t velocity_magnitude_squared = (velocity_x2 + velocity_z2);
-    scalar_t velocity_scalar = scalar_sqrt(velocity_magnitude_squared);
-
-    // Normalize the speed
-    velocity_x = scalar_div(velocity_x, velocity_scalar);
-    velocity_z = scalar_div(velocity_z, velocity_scalar);
-
-    // Clamp magnitude
-    const scalar_t curr_drag = (self->is_grounded) ? (walking_drag * dt_ms) : ((walking_drag * dt_ms) / jump_drag_divider);
-    if (velocity_scalar > walking_max_speed) {
-        velocity_scalar = walking_max_speed - curr_drag;
+void handle_stick_input(player_t* self, const scalar_t dt) {
+    scalar_t curr_acceleration = scalar_mul(walking_acceleration, dt);
+    if (!self->is_grounded) {
+        curr_acceleration = scalar_div(curr_acceleration, air_acceleration_divider);
     }
-    // Apply drag
-    else if (velocity_scalar > curr_drag) {
-        velocity_scalar -= curr_drag;
+
+    // Moving forwards and backwards
+    const vec3_t forward = (vec3_t) {
+        trig_sin(self->transform.rotation.y),
+        0,
+        trig_cos(self->transform.rotation.y)
+    };
+
+    const vec3_t right = (vec3_t) {
+        forward.z,
+        0,
+        -forward.x
+    };
+
+    const vec2_t stick_left = (vec2_t) {
+        SCALAR((float)input_left_stick_x(0) / 127.0f),
+        SCALAR((float)input_left_stick_y(0) / 127.0f)
+    };
+
+    const vec2_t stick_right = (vec2_t) {
+        SCALAR((float)input_right_stick_x(0) / 127.0f),
+        SCALAR((float)input_right_stick_y(0) / 127.0f)
+    };
+
+    const vec2_t mouse_delta = (vec2_t) {
+        SCALAR(input_mouse_movement_x()),
+        SCALAR(input_mouse_movement_y())
+    };
+
+    printf("forward: "); vec3_debug(forward);
+    printf("right: "); vec3_debug(right);
+    printf("stick_left: "); vec2_debug(stick_left);
+    printf("stick_right: "); vec2_debug(stick_right);
+    printf("mouse_delta: "); vec2_debug(mouse_delta);
+    printf("scalar_mul(scalar_mul(-stick_right.y, dt), stick_sensitivity): "); scalar_debug(scalar_mul(scalar_mul(-stick_right.y, dt), stick_sensitivity));
+
+    // Moving horizontally
+    self->velocity = vec3_add(self->velocity, vec3_muls(forward, scalar_mul(stick_left.y, walking_acceleration * PLAYER_VELOCITY_PRECISION)));
+    self->velocity = vec3_add(self->velocity, vec3_muls(right, scalar_mul(stick_left.x, walking_acceleration * PLAYER_VELOCITY_PRECISION)));
+
+    // Looking up and down
+    self->transform.rotation.x += scalar_mul(scalar_mul(-stick_right.y, dt), stick_sensitivity);
+    self->transform.rotation.x += scalar_mul(scalar_mul(-mouse_delta.y, dt), mouse_sensitivity);
+    self->transform.rotation.x = scalar_clamp(self->transform.rotation.x, SCALAR(-0.22), SCALAR(0.22));
+
+    // Looking left and right
+    self->transform.rotation.y += scalar_mul(scalar_mul(-stick_right.x, stick_sensitivity), dt);
+    self->transform.rotation.y += scalar_mul(scalar_mul(-mouse_delta.x, mouse_sensitivity), dt);
+}
+
+void handle_drag(player_t* self, const scalar_t dt) {
+    scalar_t curr_drag = scalar_mul(drag, dt);
+    if (self->is_grounded) {
+        curr_drag = scalar_div(drag, jump_drag_divider);
+    }
+
+    const scalar_t length = vec3_magnitude(self->velocity);
+    const vec3_t dir = vec3_divs(self->velocity, length);
+    if (length > (walking_max_speed * PLAYER_VELOCITY_PRECISION)) {
+        const scalar_t preserve_y = self->velocity.y;
+        self->velocity = vec3_muls(dir, length);
+        self->velocity.y = preserve_y;
+    }
+    else if (length > (curr_drag * PLAYER_VELOCITY_PRECISION)) {
+        const scalar_t preserve_y = self->velocity.y;
+        self->velocity = vec3_sub(self->velocity, vec3_muls(dir, curr_drag * PLAYER_VELOCITY_PRECISION));
+        self->velocity.y = preserve_y;
     }
     else {
-        velocity_scalar = 0;
+        self->velocity.x = 0;
+        self->velocity.z = 0;
     }
-
-    // Apply new magnitude to the velocity
-    velocity_x = scalar_mul(velocity_x, (velocity_scalar));
-    velocity_z = scalar_mul(velocity_z, (velocity_scalar));
-
-    // Put it back in the velocity component
-    self->velocity.x = velocity_x;
-    self->velocity.z = velocity_z;
 }
 
 int was_grounded = 0;
@@ -201,114 +183,56 @@ void handle_jump(player_t* self) {
     was_grounded = self->is_grounded;
 }
 
-void handle_movement(player_t* self, level_t* level, const int dt_ms) {
-    (void)level;
-    // Move the player, ask questions later
-    self->position.x += self->velocity.x * dt_ms / PLAYER_VELOCITY_PRECISION;
-    self->position.z += self->velocity.z * dt_ms / PLAYER_VELOCITY_PRECISION;
-
-    for (size_t i = 0; i < 2; ++i) {
-        rayhit_t hit = {};
-#ifndef _DEBUG_CAMERA
-        // Collide
-        const vertical_cylinder_t cyl = {
-            .bottom = (vec3_t){self->position.x, self->position.y - eye_height - ONE + step_height, self->position.z},
-            .height = eye_height + ONE - step_height,
-            .radius = player_radius,
-            .is_wall_check = 1,
-        };
-        // bvh_intersect_vertical_cylinder(level_bvh, cyl, &hit);
-
-        const size_t n_active_aabb = entity_get_n_active_aabb();
-        for (size_t i = 0; i < n_active_aabb; ++i) {
-            const entity_collision_box_t* const box = entity_get_aabb_queue_entry(i);
-            rayhit_t curr_hit;
-            curr_hit.distance = INT32_MAX;
-            if (!box->is_solid && !box->is_trigger) continue;
-
-            // int intersect = vertical_cylinder_aabb_intersect_fancy(&box->aabb, cyl, &curr_hit);
-            // if (intersect && box->is_trigger) {
-            //     entity_send_player_intersect(box->entity_index, self);
-            // }
-            // if (!box->is_solid) continue;
-            // if (!intersect) continue;
-            // if (curr_hit.distance < hit.distance) memcpy(&hit, &curr_hit, sizeof(rayhit_t));
-        }
-#else
-        (void)level;
-        hit.distance = INT32_MAX;
-#endif
-
-        // Did we hit anything?
-        if (!is_infinity(hit.distance) && hit.distance > 0) {
-            // Eject player out of geometry
-            const vec3_t amount_to_eject = (vec3_t){
-                scalar_mul(hit.normal.x, player_radius - hit.distance),
-                scalar_mul(hit.normal.y, hit.distance),
-                scalar_mul(hit.normal.z, player_radius - hit.distance),
-            };
-            self->position = vec3_add(self->position, amount_to_eject);
-
-            // Absorb penetration force (lol)
-            const scalar_t velocity_length = scalar_sqrt(vec3_magnitude_squared(self->velocity));
-            const vec3_t velocity_normalized = vec3_divs(self->velocity, velocity_length);
-            const vec3_t undesired_motion = vec3_muls(hit.normal, vec3_dot(velocity_normalized, hit.normal));
-            const vec3_t desired_motion = vec3_sub(velocity_normalized, undesired_motion);
-            self->velocity = vec3_muls(desired_motion, velocity_length);
-        }
-    }
-    self->position.y += self->velocity.y * dt_ms;
-}
-
-void player_update(player_t* self, level_t* level, const int dt_ms, const int time_counter) {
-    if (!self) return;
-    if (!level) return;
-
-    const vec3_t player_right = (vec3_t) {
-        -trig_cos(self->rotation.y),
-        0,
-        +trig_sin(self->rotation.y),
-    };
-    audio_update_listener(self->position, player_right);
-
-    self->is_grounded = 0;
-#ifndef _DEBUG_CAMERA
-    apply_gravity(self, dt_ms);
-#endif
-    // check_ground_collision(self, level_bvh, dt_ms);
-    collide(self, level);
-    handle_stick_input(self, dt_ms);
-    handle_drag(self, dt_ms);
-#ifndef _DEBUG_CAMERA
-    handle_jump(self);
-#endif
-    handle_movement(self, level, dt_ms);
-
+void update_transform(player_t* self, scalar_t dt, scalar_t time_counter) {
     const vec2_t vel_2d = {self->velocity.x, self->velocity.z};
-    const scalar_t speed_1d = vec2_magnitude(vel_2d) / PLAYER_VELOCITY_PRECISION;
+    const scalar_t speed_1d = vec2_magnitude(vel_2d);
 
-#ifdef _DEBUG_CAMERA
+    #ifdef _DEBUG_CAMERA
     (void)time_counter;
-    self->position.y += 1000 * ((dt_ms * (input_held(PAD_UP, 0) != 0)) - (dt_ms * (input_held(PAD_DOWN, 0) != 0)));
+    // Moving up and down
+    if (input_held(PAD_SQUARE, 0)) self->position.y -= scalar_mul(SCALAR(100), dt);
+    if (input_held(PAD_CROSS, 0))  self->position.y += scalar_mul(SCALAR(100), dt);
     self->transform.position.y = self->position.y;
 
-#else
-    self->transform.position.y = self->position.y + trig_sin(time_counter * 12) * speed_1d / 64;
+    #else
+    self->transform.position.y = self->position.y + scalar_mul(trig_sin(time_counter * 4), speed_1d / 64);
 
-    self->footstep_timer += dt_ms;
+    self->footstep_timer += dt;
     if (self->footstep_timer >= FOOTSTEP_TIMER_MAX) {
         self->footstep_timer -= FOOTSTEP_TIMER_MAX;
         if (self->is_grounded && (speed_1d > ONE / 16)) {
             audio_play_sound(random_range(sfx_footstep1, sfx_footstep7 + 1), 0, 0, (vec3_t){}, 1);
         }
     }
-#endif
+    #endif
 
     self->transform.position.x = self->position.x;
-    self->transform.position.y = self->position.y + trig_sin(time_counter * 12) * speed_1d / 64;
     self->transform.position.z = self->position.z;
 
     self->transform.rotation.x = self->rotation.x;
     self->transform.rotation.y = self->rotation.y;
     self->transform.rotation.z = self->rotation.z;
+}
+
+void player_update(player_t* self, level_t* level, const scalar_t dt, const scalar_t time_counter) {
+    if (!self) return;
+    if (!level) return;
+
+    printf("dt: "); scalar_debug(dt);
+
+    const vec3_t player_right = (vec3_t) {-trig_cos(self->rotation.y),0,+trig_sin(self->rotation.y)};
+    audio_update_listener(self->position, player_right);
+
+    self->is_grounded = 0;
+    apply_gravity(self, dt);
+    handle_stick_input(self, dt);
+    handle_drag(self, dt);
+    handle_jump(self);
+
+    printf("velocity: "); vec3_debug(vec3_divs(self->velocity, SCALAR(PLAYER_VELOCITY_PRECISION)));
+
+    self->position = vec3_add(self->position, vec3_muls(vec3_divs(self->velocity, SCALAR(PLAYER_VELOCITY_PRECISION)), dt));
+
+    collide(self, level);
+    update_transform(self, dt, time_counter);
 }
--- source/player.h
+++ source/player.h
@@ -6,8 +6,8 @@ extern "C" {
 #endif
 
 #include "math/scalar.h"
-#include "collision.h"
-#include "renderer.h"
+#include "structs.h"
+#include "level.h"
 #include "math/vec3.h"
 
 #define MAX_HEALTH 100
@@ -19,7 +19,7 @@ typedef struct {
     vec3_t position;
     vec3_t velocity;
     vec3_t rotation;
-    int footstep_timer;
+    scalar_t footstep_timer;
     int ground_entity_id_prev; // -1 = no entity
     int ground_entity_id_curr; // -1 = no entity
     transform_t ground_entity_prev;
@@ -33,27 +33,26 @@ typedef struct {
     unsigned int is_grounded : 1;
 } player_t;
 
-#define PLAYER_VELOCITY_PRECISION 16
+#define PLAYER_VELOCITY_PRECISION 4
 const static scalar_t eye_height = SCALAR(200);
 const static scalar_t player_height = SCALAR(230);
 const static scalar_t player_radius = SCALAR(320);
 const static int32_t step_height = SCALAR(100);
-const static scalar_t terminal_velocity_down = SCALAR(-5.0);
-const static int32_t terminal_velocity_up = 40000;
-const static scalar_t gravity = SCALAR(-0.1);
-const static scalar_t walking_acceleration = SCALAR(8.0);
-const static scalar_t air_acceleration_divider = SCALAR(0.5);
-const static scalar_t walking_max_speed = SCALAR(15.0);
-const static scalar_t stick_sensitivity = SCALAR(0.25);
-const static scalar_t mouse_sensitivity = SCALAR(2.0);
-const static scalar_t walking_drag = SCALAR(0.2);
-const static scalar_t vertical_drag = SCALAR(0.2);
-const static int32_t jump_drag_divider = 4;
-const static int32_t initial_jump_velocity = 1200;
+const static scalar_t terminal_velocity_down = SCALAR(-5000.0);
+const static scalar_t terminal_velocity_up = SCALAR(500.0);
+const static scalar_t gravity = SCALAR(-1000);
+const static scalar_t walking_acceleration = SCALAR(50);
+const static scalar_t air_acceleration_divider = SCALAR(2);
+const static scalar_t walking_max_speed = SCALAR(80);
+const static scalar_t stick_sensitivity = SCALAR(1.0);
+const static scalar_t mouse_sensitivity = SCALAR(0.2);
+const static scalar_t drag = SCALAR(10);
+const static scalar_t jump_drag_divider = SCALAR(2);
+const static int32_t initial_jump_velocity = SCALAR(750);
 const static int32_t jump_ground_threshold = 4000;
 
 void player_init(player_t* player, vec3_t position, vec3_t rotation, int health, int armor, int ammo);
-void player_update(player_t* self, level_t* level, const int dt_ms, const int time_counter);
+void player_update(player_t* self, level_t* level, const scalar_t dt, const scalar_t time_counter);
 
 #ifdef __cplusplus
 }
